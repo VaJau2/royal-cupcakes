defade2ae24a160d3620c15507b29621f4fcb3e9
VaJau2 <vadim206-1998@yandex.ru>
2023-05-22 16:26:39 +0500

    CAKE-28 | Мини-карта для воров

---
+++ assets/scripts/characters/player/MinimapItem.cs
@@ -0,0 +1,32 @@
+using Godot;
+using RoyalCupcakes.Interface;
+using RoyalCupcakes.System;
+
+namespace RoyalCupcakes.Characters.Player;
+
+public partial class MinimapItem : Node
+{
+	private Character player;
+	private MiniMap miniMap;
+	
+	public override void _Ready()
+	{
+		miniMap = GetNode<MiniMap>("/root/Main/Level/Scene/canvas/miniMap");
+		player = GetParent<Character>();
+
+		if (player.IsMultiplayerAuthority())
+		{
+			if (player.Team == Team.Thief)
+			{
+				miniMap.SetVisible(player);
+			}
+		}
+		else
+		{
+			if (player.Team == Team.Guard)
+			{
+				miniMap.SpawnMarker(player);
+			}
+		}
+	}
+}
---
+++ assets/scripts/interface/MiniMap.cs
@@ -0,0 +1,70 @@
+using Godot;
+using Godot.Collections;
+
+namespace RoyalCupcakes.Interface;
+
+public partial class MiniMap : Control
+{
+	private const float Zoom = 0.025f;
+	private Control markersParent;
+	private Sprite2D enemyMarker;
+
+	private Vector2 gridScale;
+	private Dictionary<Node3D, Sprite2D> markers = new();
+	private Node3D player;
+	
+	public void SetVisible(Node3D newPlayer)
+	{
+		player = newPlayer;
+		Visible = true;
+	}
+
+	public void SpawnMarker(Node3D character)
+	{
+		if (enemyMarker.Duplicate() is not Sprite2D newMarker) return;
+		markersParent.AddChild(newMarker);
+		newMarker.Visible = true;
+		markers.Add(character, newMarker);
+	}
+
+	public override void _Ready()
+	{
+		markersParent = GetNode<Control>("mask");
+		enemyMarker = markersParent.GetNode<Sprite2D>("enemy");
+
+		var viewportZoom = GetViewportRect().Size * Zoom;
+		gridScale = markersParent.GetRect().Size / viewportZoom;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (!Visible) return;
+		foreach (var markerData in markers)
+		{
+			if (IsInstanceValid(markerData.Key))
+			{
+				UpdateMarker(markerData.Key);
+			}
+			else
+			{
+				markerData.Value.QueueFree();
+				markers.Remove(markerData.Key);
+			}
+		}
+	}
+
+	private void UpdateMarker(Node3D character)
+	{
+		markers[character].Position = GetMarkerPos(character);
+	}
+	
+	private Vector2 GetMarkerPos(Node3D character)
+	{
+		var character2DPos = Get2DPos(character.GlobalPosition);
+		var player2DPos = Get2DPos(player.GlobalPosition);
+		var posByPlayer = character2DPos - player2DPos;
+		return posByPlayer * gridScale + markersParent.GetRect().Size / 2;
+	}
+
+	private static Vector2 Get2DPos(Vector3 originPos) => new(originPos.X, originPos.Z);
+}
